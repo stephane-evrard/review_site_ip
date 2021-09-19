ce5e2251db6da2fef8369dfcddac95f953d1c371
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-20 00:49:32 +0300

    project list apiview

--- reviewz/awwards/views.py
+++ reviewz/awwards/views.py
@@ -91,3 +91,18 @@ def user_profiles(request):
         form = ProfileUpdateForm()
     
     return render(request, 'registration/profile.html', {"form":form, "projects":projects})
+
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_project = Projects.objects.all()
+        serializers = ProjectSerializer(all_project, many=True)
+        return Response(serializers.data)
+    
+    
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)
+    
+    
