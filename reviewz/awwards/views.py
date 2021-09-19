121a8ea5cd6ec118a67e03cbce2f1814ce054d8e
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-20 00:48:47 +0300

    user profile view

--- reviewz/awwards/views.py
+++ reviewz/awwards/views.py
@@ -73,3 +73,21 @@ def new_project(request):
     else:
         form = NewProjectForm()
     return render(request, 'new-project.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def user_profiles(request):
+    current_user = request.user
+    Author = current_user
+    projects = Projects.get_by_author(Author)
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+        
+    else:
+        form = ProfileUpdateForm()
+    
+    return render(request, 'registration/profile.html', {"form":form, "projects":projects})
