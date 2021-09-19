3599cb3b7c4d370e176b909716052d0ab87c03e9
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-20 00:34:09 +0300

    class profile update form

--- reviewz/awwards/forms.py
+++ reviewz/awwards/forms.py
@@ -13,4 +13,12 @@ class NewProjectForm(forms.ModelForm):
         widgets = {
           'project_description': forms.Textarea(attrs={'rows':4, 'cols':10,}),
         }
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        widgets = {
+          'bio': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+        }
         
