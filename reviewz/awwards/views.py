947e12e10190ec7dc3138ee5e989df4e802d3130
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-09-20 00:46:26 +0300

    search project view

--- reviewz/awwards/views.py
+++ reviewz/awwards/views.py
@@ -34,4 +34,17 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'registration/registration_form.html', {'form':form})
-    
+
+@login_required(login_url='/accounts/login/')
+def search_projects(request):
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_projects = Projects.search_projects(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message":message,"projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
+
